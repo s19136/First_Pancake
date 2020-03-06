f4b30ecc8d777f95fd932380c1d0d57ef481bfe9
Dmitri Vaitkun <s19136@pjwstk.edu.pl>
2020-03-06 09:29:08 +0100

    initial commit

---
+++ first_solution/first_project/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Net.Http;
+using System.Text.RegularExpressions;
+using System.Threading.Tasks;
+
+namespace first_project
+{
+    class Program
+    {
+        public static async Task Main(string[] args)
+        {
+            var websiteUrl = args[0];
+            var httpClient = new HttpClient();
+            var response = await httpClient.GetAsync(websiteUrl);
+            if(response.IsSuccessStatusCode)
+            {
+                var htmlContent = await response.Content.ReadAsStringAsync();
+
+                var regex = new Regex("[a-z]+[a-z0-9]*@[a-z0-9]+\\.[a-z]+", RegexOptions.IgnoreCase);
+
+                var emailAddresses = regex.Matches(htmlContent);
+                foreach(var emailAddress in emailAddresses)
+                {
+                    Console.WriteLine(emailAddress.ToString());
+                }
+            }
+            Console.ReadKey();
+        }
+    }
+}
